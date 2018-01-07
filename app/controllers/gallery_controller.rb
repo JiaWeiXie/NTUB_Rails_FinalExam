2adf7a95639ad17a5a893438eb02cd01519b18e4
謝佳瑋 <x5758x@gmail.com>
2018-01-08 05:03:01 +0800

    Modify home view, Add gallery controller

---
+++ app/controllers/gallery_controller.rb
@@ -0,0 +1,31 @@
+class GalleryController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+      
+    end
+    
+    def new
+      
+    end
+    
+    def create
+      
+    end
+    
+    def edit
+      
+    end
+    
+    def update
+      
+    end
+    
+    def show
+      
+    end
+    
+    def destroy
+      
+    end
+end
